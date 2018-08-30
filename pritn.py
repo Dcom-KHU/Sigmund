54b36f4129cc9409beb0b9b2f0b0692da76fc1d1
JINHYUNGP <carpedm96@naver.com>
2018-08-30 15:36:13 +0900

    finish
    close #11

--- pritn.py
+++ pritn.py
@@ -1,6 +1,8 @@
-import datetime
+import time
+from datetime import datetime
 
-
-aaa = "30".split(',')
-bbb=map(int,aaa)
-print(bbb)
+d = 20 
+s = d* 60*60*24
+timestamp = time.mktime(datetime.today().timetuple()) + s
+p = datetime.fromtimestamp(timestamp)
+print(p)
