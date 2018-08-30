2b362264c0c4889626738bafa8f92f4252b1a640
JINHYUNGP <carpedm96@naver.com>
2018-08-30 15:36:13 +0900

    finish

--- crawling.py
+++ crawling.py
@@ -2,11 +2,11 @@ import requests
 import json
 import re
 from json import dumps, loads, JSONEncoder, JSONDecoder
+import time
+from datetime import datetime
 import datetime
 
 now = datetime.datetime.now()
-
-
 def strToInt(str):
     result = 0
     result = int(str)
@@ -27,7 +27,6 @@ URL = "https://www.wevity.com/?c=find&s=1&gub=1&cidx=21"
 html = get_html(URL)
 soup = BeautifulSoup(html, 'html.parser')
 
-#print(soup)
 
 
 
@@ -38,8 +37,7 @@ titles = bbs[0].findAll("div", {"class": "tit"})
 organs = bbs[0].findAll("div", {"class": "organ"})
 remaindate = bbs[0].findAll("div", {"class": "day"})
 
-d = datetime.time
-print(d)
+
 challenge = []
 for i in range(1, len(titles)):
     
@@ -50,12 +48,9 @@ for i in range(1, len(titles)):
     
     S = remdate 
     numbers = re.findall("\d+",S)
-    
+    endDate = now + datetime.timedelta(days=int(numbers[0])) 
 
-    endDate = now + datetime.timedelta(days=1)
-
-    j2 = {"title": title,"organism":organ,"Date":endDate}
-
+    j2 = {"title": title,"organism":organ,"Date":str(endDate)}
     challenge.append(j2)
 print(challenge)
 print(json.dumps({"challenge":challenge}, indent=2))
